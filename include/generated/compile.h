c9b6ebf044deaa5e76f437c35189029d79c8163a
Jake Tesler <jake.tesler@gmail.com>
2013-10-24 03:15:42 -0400

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Wed Oct 16 16:48:05 EDT 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "cmu-879370.wv.cc.cmu.edu"
+#define LINUX_COMPILER "gcc version 4.7.2 (GCC) "
